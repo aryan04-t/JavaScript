b7ae25d3b882e09d8121801d542e54427e88846b
Aryan Tomar <aryantomar5000@gmail.com>
2023-12-19 23:45:51 +0530

    Dynamically & Statically Typed Programming Languages

---
+++ 01_Dynamically_Typed_and_Statically_Typed_Programming_Languages.js
@@ -0,0 +1,63 @@
+/*
+
+
+# Dynamically Typed Programming Language:
+
+
+- Flexible Variable Types: 
+--> In a dynamically typed language, you don't need to declare the data type of a variable explicitly. The type of a variable is determined at runtime, allowing you to change the type of a variable as the program runs.
+
+
+- Runtime Type Checking: 
+--> Dynamically typed languages perform type checking during runtime. This means that the interpreter or runtime system checks the types of variables as the program is executed, catching type-related errors on the fly.
+
+
+- Less Code, More Readable: 
+--> Because you don't have to declare variable types explicitly, dynamic languages often require less code. This can make the code more concise and readable, as you focus on the logic rather than dealing with type declarations.
+
+
+- Examples:
+--> Python and JavaScript are example of "Dynamically Typed Programming Langugages". 
+
+
+
+// - - - - - - - - - - 
+
+
+
+# Statically Typed Programming Language:
+
+
+- Declared Variable Types: 
+--> In a statically typed language, you must explicitly declare the data type of a variable when you define it. The type is checked at compile time, before the program is run.
+
+
+- Compile-Time Type Checking: 
+--> Statically typed languages perform type checking at compile time. This means that potential type errors are caught before the program is executed, reducing the chance of runtime errors related to type mismatches.
+
+
+- Explicitness and Safety: 
+--> Statically typed languages offer a level of explicitness in code, making it clear what types of values are expected. This can enhance code safety by catching errors early in the development process.
+
+
+- Examples:
+--> Java and C++ are example of "Statically Typed Programming Langugages". 
+
+
+
+*/
+
+
+
+/*
+
+
+# References Cited: 
+
+
+* ChatGPT: 
+https://chat.openai.com/ 
+
+
+
+*/
