af8bd3bb89097c2f438f29f759f40cd3f45ede99
Aryan Tomar <aryantomar5000@gmail.com>
2023-12-22 23:58:11 +0530

    Understanding Datatypes in JavaScript

---
+++ 04_Datatypes_in_JavaScript.js
@@ -0,0 +1,54 @@
+/*
+
+
+
+# There are 4 Basic / Primitive Datatypes in JavaScript: 
+
+1. Strings 
+2. Booleans 
+3. Numbers -> (int, float, NaN) 
+4. Undefined 
+
+
+
+
+# There are 2 Complex / Non-Primitive Datatypes in JavaScript: 
+
+1. Objects 
+    A) Key-Value Pair Objects 
+    B) Arrays 
+    C) Date 
+    D) BitInt 
+    E) null 
+
+2. Functions 
+
+
+
+
+$ Operators like ( + - * / ) do not have any datatype 
+
+
+
+*/
+
+
+
+
+/*
+
+
+@ BigInt:
+--> BigInt is a built-in object in JavaScript that provides a way to represent whole numbers larger than 2^53-1. 
+
+
+@ Undefined:
+--> represents undefined value 
+
+
+@ Null: 
+--> represents no value at all 
+
+
+
+*/
