7cbf96223fe1b2b7275a62dfabd5d30dcf215b33
Aryan Tomar <aryantomar5000@gmail.com>
2023-12-22 23:57:33 +0530

    Updated Info. of "00_Introduction_to_JavaScript.js"

--- 00_Introduction_to_JavaScript.js
+++ 00_Introduction_to_JavaScript.js
@@ -2,6 +2,45 @@
 
 
 
+# History of JavaScript: 
+
+--> JavaScript was created by Brendan Eich while he was working at Netscape Communications Corporation in 1995. 
+--> 1st JavaScript was named "Mocha", after some time language was renamed to "LiveScript", and finally, JavaScipt was named "JavaScript". 
+--> JavaScript was named "JavaScript" due to marketing purpose, because Java was a popular in those times when JavaScript was striving to gain popularity 
+--> Java and JavaScript have no relations, both of them are totally different and they're totally unrelated programming languages. 
+--> The naming of JavaScript was influenced by marketing and business considerations rather than technical similarities. 
+
+
+
+// - - - - - - - - - - 
+
+
+
+# JavaScript as a Scripting and Prototype based Programming Language: 
+
+
+$ JS is a scripting language which supports object-oriented programming, but it doesn't strictly follow classical Object-Oriented Programming (OOP) concepts like some other languages such as Java or C++. Instead, JavaScript is a prototype-based language.
+
+$ In prototype-based programming, objects can serve as prototypes for other objects, and inheritance is achieved through the prototype chain. 
+
+
+
+// - - - - - - - - - - 
+
+
+
+@ What is a Scripting Language: 
+
+--> A scripting language is a type of programming language that is interpreted rather than compiled. 
+--> In contrast to compiled languages, where the code is translated into machine code before execution, scripting languages are executed by an interpreter at runtime. 
+--> This means that the source code is read and executed line by line, without the need for a separate compilation step.
+
+
+
+// - - - - - - - - - - 
+
+
+
 # JavaScript as an Interpreted Language:
 
 
@@ -9,14 +48,19 @@
 
 - Node.js is an open-source, cross-platform JavaScript runtime environment that executes JavaScript code outside of a browser. 
 
+- Next.js was created by "Guillermo Rauch", a software engineer at Vercel, and is maintained by a team of developers at Vercel. It is an open-source project, and you can find the source code on GitHub.
+
 - JavaScript is a dynamically typed language, allowing flexibility in variable and constant declarations without the need to specify data types explicitly. 
 
 - In contrast to languages like C++, JavaScript does not require a main() method as an entry point. This is due to the fact that JavaScript is not a compiled language but an interpreted one. 
 
 
 
+// - - - - - - - - - - 
 
-# JavaScript vs. C++ and Java:
+
+
+# How C++ and Java are different from JavaScript: 
 
 
 - C++ is a strictly typed and compiled language. It requires a main() method as an entry point and is designed for direct machine code execution.
@@ -44,14 +88,22 @@ console.log("Hello, World!!");
 
 
 
+// - - - - - - - - - - 
+
+
+
 # Single-Threaded Nature of JavaScript:
 
-JavaScript is inherently single-threaded, meaning it processes one task at a time in a single sequence. This is in contrast to C++ and Java, which support multithreading.
+--> JavaScript is inherently single-threaded, meaning it processes one task at a time in a single sequence. This is in contrast to C++ and Java, which support multithreading.
 
+
+
+// - - - - - - - - - - 
 
 
 
 # Multithreading and Cores in a PC:
+
 
 - Multithreading involves executing multiple threads (independent sequences of instructions) concurrently.
 
@@ -61,8 +113,12 @@ JavaScript is inherently single-threaded, meaning it processes one task at a tim
 
 
 
+// - - - - - - - - - - 
+
+
 
 # Challenges with Single-Threaded Nature:
+
 
 - Single-threaded nature limits the concurrent processing capability of JavaScript.
 
@@ -70,6 +126,9 @@ JavaScript is inherently single-threaded, meaning it processes one task at a tim
 
 - Scalability challenges arise as the application may struggle to efficiently utilize modern, multicore processors.
 
+
+
+// - - - - - - - - - - 
 
 
 
@@ -91,6 +150,7 @@ Overall, while JavaScript's single-threaded nature poses challenges for scalabil
 
 
 /*
+
 
 
 # References Cited: 
